19a2c8c1839eeaf78b884725a69b521b73ec2ce2
Elisabeth <viilaselisabeth1@gmail.com>
2023-11-11 11:15:08 +0200

    adds access_modifiers

---
+++ src/main/java/access_modifiers/p1/C1.java
@@ -0,0 +1,24 @@
+package access_modifiers.p1;
+
+public class C1 {
+    // public is access modifier or visibility
+    // accessible from any other classes
+    public int publicField;
+
+    // default access/visibility modifier (package - private or package-access)
+    // accessible only by any class in the same package
+    int defaultField;
+
+    // private modifier
+    // accessible only by the class itself
+    private int privateField;
+
+    // protected modifier
+    // accessible only by the class itself  AND classes in the same package AND its subclasses
+    protected int protectedField;
+
+    public void publicMethod() {}
+    void defaultMethod() {}
+    private void privateMethod() {}
+    protected void protectedMethod() {}
+}
